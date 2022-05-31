e2a0a2eea4d8b3b2fb967f45dca65f22cfcda3cf
Njoku <njokunneoma1999@gmail.com>
2022-05-31 14:29:24 +0200

    edited file content

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
-able File  3 lines (3 sloc)  95 Bytes
-   
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(c), end="")
+for a in "abcdefghijklmnopqrstuvwxyz":
+    print('{}'.format(a), end="")
